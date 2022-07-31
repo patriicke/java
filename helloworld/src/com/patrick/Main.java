678422314649bb5dbbfaa901a1a41dd41998c11b
patrick-n4 <patrickndayambaje4@gmail.com>
2022-07-31 15:37:54 +0200

    I have added the primitive data types

--- helloworld/src/com/patrick/Main.java
+++ helloworld/src/com/patrick/Main.java
@@ -1,8 +1,19 @@
 package com.patrick;
 public class Main {
     public static void main(String[] args){
+        //Bytes
         byte age = 20;
+        //Long integers
         long viewCount = 3_123_456_789L;
-        System.out.println(age);
+        //Floats
+        float price = 10.1F;
+        //Characters
+        char letter = 'A';
+        //Strings
+        String userName = "Patrick";
+        String message = "Hello Patrick hope that tomorrow you will come to work";
+        //Booleans
+        boolean isTrue = true;
+        System.out.println(isTrue);
     }
 }
