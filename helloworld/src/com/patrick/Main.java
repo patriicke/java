17dbec826b2b7b9d5b562fde37d33c2a92682233
patrick-n4 <patrickndayambaje4@gmail.com>
2022-07-31 15:29:25 +0200

    I have learnt how to use long in java

--- helloworld/src/com/patrick/Main.java
+++ helloworld/src/com/patrick/Main.java
@@ -2,6 +2,7 @@ package com.patrick;
 public class Main {
     public static void main(String[] args){
         byte age = 20;
+        long viewCount = 3_123_456_789L;
         System.out.println(age);
     }
 }
