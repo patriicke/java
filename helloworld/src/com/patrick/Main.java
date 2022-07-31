88130d198f862537327e9611924876c6356ca45a
patrick-n4 <patrickndayambaje4@gmail.com>
2022-07-31 15:27:51 +0200

    I have made the byte type to store number

--- helloworld/src/com/patrick/Main.java
+++ helloworld/src/com/patrick/Main.java
@@ -1,8 +1,7 @@
 package com.patrick;
 public class Main {
     public static void main(String[] args){
-        int age = 20;
-        age = 25;
+        byte age = 20;
         System.out.println(age);
     }
 }
